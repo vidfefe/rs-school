9875782a3c64c044c1f0e35dd0037e1b2d90e565
vidfefe <malinovskaaviktoria7@gmail.com>
2025-02-09 13:37:14 +0300

    refactor: update api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,51 @@
-interface Pokemon {
+interface PokemonResponse {
+  results: { name: string; url: string }[];
+}
+
+interface ApiPokemonDetailsResponse {
+  name: string;
+  height: number;
+  weight: number;
+  sprites: { front_default: string };
+}
+
+export interface Pokemon {
   name: string;
   description: string;
   image: string;
 }
 
-export const fetchPokemon = async (pokemonName: string): Promise<Pokemon> => {
+export const fetchPokemon = async (searchValue: string): Promise<Pokemon[]> => {
   try {
     const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+      'https://pokeapi.co/api/v2/pokemon?limit=1000'
     );
     if (!response.ok) {
-      throw new Error('Failed to fetch pokemons');
+      throw new Error(`Failed to fetch Pokemons`);
     }
-    const data = await response.json();
-    console.log(data);
 
-    return {
-      name: data.name,
-      description: `Height: ${data.height}, Weight: ${data.weight}`,
-      image: data.sprites.front_default,
-    };
+    const data: PokemonResponse = await response.json();
+
+    const filteredPokemon = data.results.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    const limitedPokemon = filteredPokemon.slice(0, 12);
+
+    const results = limitedPokemon.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const details: ApiPokemonDetailsResponse = await res.json();
+
+      return {
+        name: details.name,
+        description: `Height: ${details.height}, Weight: ${details.weight}`,
+        image:
+          details.sprites.front_default ||
+          'https://placehold.co/250x250/transparent/red',
+      };
+    });
+
+    return Promise.all(results);
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
